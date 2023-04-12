f478065b93679782cc5824a0fe582d9ef4528e0c
Y0SHI0N <94875891+Y0SHI0N@users.noreply.github.com>
2023-04-12 13:06:47 +1000

    Add project files.

---
+++ Advance/AdvanceBoard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Advance
+{
+    class Cell
+    {
+        public int row {  get; private set; }
+        public int col { get; private set; }
+        public bool currentlyOccupied { get; set; }
+        public bool legalNextMove { get; set; }
+        public char value { get; set; }
+        public Cell(int x, int y)
+        {
+            row = x;
+            col = y;
+        }
+    }
+
+    class Board
+    {
+        public int Size {get; set; }
+        public Cell[,] Grid { get; set; }
+        public Board(int s = 9) 
+        {
+            Size = s;
+            Grid =new Cell[Size,Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid[i,j] = new Cell(i,j);
+                }
+            }
+        }
+
+        public void readFileToBoard(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        int rowCnt = 0;
+                        Console.WriteLine("From file:");
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            Console.WriteLine(line);
+                            for (int columnCnt = 0; columnCnt < Size; columnCnt++)
+                            {
+                                Grid[rowCnt, columnCnt].value = line[columnCnt];
+                            }
+                            rowCnt++;
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void writeBoardToFile(string path)
+        {
+            string text = "";
+
+            for (int i = 0;i < Size; i++)
+            {
+                for(int j = 0;j < Size; j++)
+                {
+                    text = text + Grid[i,j].value.ToString();
+                }
+                text = text + "\n";
+            }
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamWriter writer = new StreamWriter(path))
+                    {
+                        writer.Write(text);
+                        writer.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Advance/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace Advance
+{
+
+    internal class Program
+    {
+        public static readonly char[] legalTroopSymbols = "ZBMJSDCGzbmjsdcg.#\n".ToCharArray();
+        private static string botName = "Eudyptula";
+        private static string[] firstArg = new string[3] {"white", "black", "name"};
+
+        public static void outputError(string message)
+        {
+            Console.WriteLine("Error: {0}",message);
+        }
+
+
+        static void Main(string[] args)
+        {
+            string? errorMsg;
+            try
+            {
+                if (args.Length == 1 && args[0] == firstArg[2])
+                {
+                    Console.WriteLine($"This bot name is: {botName}");
+                }
+                else if (args.Length == 3)
+                {
+                    bool argsOneBlackOrWhite = (Array.Exists(firstArg, x => x == args[0]) && args[0] != "name");
+
+                    // check if the argument is 'white' or 'black'. if true, then proceed, otherwise abort
+                    if (argsOneBlackOrWhite == true)
+                    {
+                        // input validation for second and third argument
+                        if ((File.Exists(args[1]) && File.Exists(args[2])) == true)
+                        {
+                            Board mainBoard = new Board();
+                            mainBoard.readFileToBoard(args[1]);
+                            Console.WriteLine("Stating game....");
+                            mainBoard.writeBoardToFile(args[2]);
+                        }
+                        else
+                        {
+                            errorMsg = "The provided filepaths were invalid. Make sure the filepaths exsist and not being use by other programs";
+                            outputError(errorMsg);
+                        }
+                    }
+                    else
+                    {
+                        errorMsg = "The first argument provided was invalid. Make sure its 'white' or 'black' if you want to start the game, or 'name if you want to get the name of the bot";
+                        outputError(errorMsg);
+                    }
+                }  
+                else
+                {
+                    errorMsg = "An invalid number of argument/s were provided. Please check the provided arguments and try again";
+                    outputError(errorMsg);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine (e.ToString());
+            }
+        }
+    }
+}
